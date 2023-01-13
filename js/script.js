4aa28fa2f5b3a627d4989df605ec541f9dfdc70a
Michał Sałata <salata.michal1@gmail.com>
2023-01-13 22:41:20 +0100

    add remove buttons

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 {
-    const tasks = [
-        {
-            content:"",
-        }
-    ];
+    const tasks = [];
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    }
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+
+    }
     const render = () => {
         let htmlString = "";
 
@@ -13,28 +21,29 @@
             <li 
             class="list__item${task.done ? " list__item--done" : ""}"
         >
+            <button class="tasks__button js-remove">🗑<button>
             ${task.content}
             </li>
             `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        })
     };
-
-    const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-        });
-
-        render();
-    }
 
     const onFormSubmit = ((event) => {
         event.preventDefault();
 
         const newTaskContent = document.querySelector(".js-newTask").value.trim();
 
-        if(newTaskContent === "") {
+        if (newTaskContent === "") {
             return;
         }
 
