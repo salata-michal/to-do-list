7a3d5edcdb3c4dd5698babf9a708e69d704bb33e
Michał Sałata <salata.michal1@gmail.com>
2023-01-13 21:26:51 +0100

    adding new tasks

---
+++ js/script.js
@@ -0,0 +1,54 @@
+{
+    const tasks = [
+        {
+            content:"",
+        }
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li 
+            class="list__item${task.done ? " list__item--done" : ""}"
+        >
+            ${task.content}
+            </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    }
+
+    const onFormSubmit = ((event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if(newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    })
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
+
+    };
+
+    init();
+}
